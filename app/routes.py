d967fd3c602da41b91cca9b647ba7096037513dd
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-08 14:18:40 +0600

    add operator if in templates/добавление условного опереатора в шаблоны

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,5 @@ from flask import render_template
 @blog.route('/')
 @blog.route('/index')
 def index():
-    user = {'username': 'Galkin Andrey'}
-    return render_template('index.html', titel='Home', user=user) # user=user['username'] / {{user}}
+    user = {'username': 'Andrey'}
+    return render_template('index.html', title='Home', user=user) # user=user['username'] / {{user}}
