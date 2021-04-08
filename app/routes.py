30676de995edc1f091083a54c5606217aecdf615
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-08 15:23:10 +0600

    add operator for in templates/Добавление цыклав шадлоны

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,19 @@ from flask import render_template
 @blog.route('/')
 @blog.route('/index')
 def index():
-    user = {'username': 'Andrey'}
-    return render_template('index.html', title='Home', user=user) # user=user['username'] / {{user}}
+    user = {'username': 'Андрей Галкин'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+            }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts) # user=user['username'] / {{user}}
