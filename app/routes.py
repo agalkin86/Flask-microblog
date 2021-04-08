09cebb28d3cf481a94a6a6575bc3f8d2bac8532c
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-08 17:36:00 +0600

    add routes formlogin/добваляем представление вормы входа

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from app import blog
 from flask import render_template
+from app.forms import LoginForm
 
 @blog.route('/')
 @blog.route('/index')
@@ -21,3 +22,8 @@ def index():
             }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts) # user=user['username'] / {{user}}
+
+@blog.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
