bbce079c6588b3e68182fefaa431f05adca439e7
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-09 13:17:04 +0600

    edit route/редактирование связей, навигация

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from app import blog
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @blog.route('/')
@@ -28,5 +28,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
