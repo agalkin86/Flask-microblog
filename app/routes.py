e4f566850e8231f165c56206a25b45821201d480
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-09 11:28:01 +0600

    get data form, validate, errors/Получение данных фомы, валидацияя и ошибки

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from app import blog
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @blog.route('/')
@@ -23,7 +23,10 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts) # user=user['username'] / {{user}}
 
-@blog.route('/login')
+@blog.route('/login', methods = ['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
