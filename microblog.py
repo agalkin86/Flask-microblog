48bde986d38e0183f64f5537357b9bf2ae80e05b
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-12 11:25:16 +0600

    config flask shell/конфигурирование flask shell

--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from app import blog
+from app import blog, db
+from app.models import User, Post
+
+
+@blog.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
