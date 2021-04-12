af88e5223d95fa6ea8a1d97115d460ea1b2fbb3c
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-12 13:04:17 +0600

    config flask-login/подключение flask-lodin к проекту

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 blog = Flask(__name__)
 blog.config.from_object(Config)
 db = SQLAlchemy(blog)
 migrate = Migrate(blog,db)
+login = LoginManager(blog)
 
 from app import routes, models
