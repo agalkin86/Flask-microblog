da755c1adfed91e0d4d4139dc18cf69f230e6c0a
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-21 16:42:35 +0600

    add logging email and file/добавление логгирования на email и логфайл

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler,RotatingFileHandler
+import os
 
 blog = Flask(__name__)
 blog.config.from_object(Config)
@@ -10,5 +13,30 @@ db = SQLAlchemy(blog)
 migrate = Migrate(blog,db)
 login = LoginManager(blog)
 login.login_view = 'login'
+
+if not blog.debug:
+    if blog.config['MAIL_SERVER']:
+        auth = None
+        if blog.config['MAIL_USERNAME'] or blog.config['MAIL_PASSWORD']:
+            auth = (blog.config['MAIL_USERNAME'], blog.config['MAIL_PASSWORD'])
+        secure = None
+        if blog.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(blog.config['MAIL_SERVER'], blog.config['MAIL_PORT']),
+            fromaddr='no-reply@' + blog.config['MAIL_SERVER'],
+            toaddrs=blog.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        blog.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    blog.logger.addHandler(file_handler)
+    blog.logger.setLevel(logging.INFO)
+    blog.logger.info('Microblog startup')
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -2,7 +2,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    
+    # настройка безовасности для форм
     SECRET_KEY = os.environ.get('SECRET_KEY_FLASK') or 'you-will-never-guess'
 
+    # настройки подключения к БД
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_FLASK') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # настройки для отправки ошибок на email
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['galkinandrey86@gmail.com']
