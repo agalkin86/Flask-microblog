a473db061c4aa9ba08cfd090927c7876eb6bdc9f
Galkin Andrey <galkinandrey86@gmail.com>
2021-04-21 17:15:26 +0600

    add validate editprofile/Добавлена валидация данных в форму редактирования профиля

--- app/forms.py
+++ app/forms.py
@@ -33,3 +33,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username/Пользователь', validators=[DataRequired()])
     about_me = TextAreaField('About me/Обо мне', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit/Сохранить')
+
+    def __init__(self,original_username,*args,**kwargs):
+        super(EditProfileForm, self).__init__(*args,**kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.original_username).first()
+            if user is not None:
+                raise ValidationError('Please use a different username/Пожалуйста, используйте другое имя пользователя')
